1e1e3ebf239ab666250c58d0b1969203b842530e
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-07 17:01:16 -0500

    Implement quicksort

--- quicksort.ts
+++ quicksort.ts
@@ -1,15 +1,21 @@
 export default function quicksort(arr: number[]): number[] {
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  // If so, it's already sorted: return
+  if (arr.length <= 1) return arr;
 
   // Pick the first value as the pivot
+  const pivot = arr[0]!;
 
   // Orient the pivot so that...
-    // every number smaller than the pivot is to the left
-    // every number larger (or equal) than the pivot is to the right
+  // every number smaller than the pivot is to the left
+  // every number larger (or equal) than the pivot is to the right
+  let [left, right] = [arr.filter(n => n < pivot), arr.filter(n => n > pivot)];
 
   // Recursively sort the left
+  left = quicksort(left);
   // Recursively sort the right
+  right = quicksort(right);
 
   // Return the left, pivot and right in sorted order
+  return [...left, pivot, ...right];
 }
